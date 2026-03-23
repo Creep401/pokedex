cb20c6e1ea582f12ed8ebc6580495447f3b58929
Creep401 <diegotadeo711@gmail.com>
2026-03-23 13:50:33 +0100

    Añadido PokedexController

--- src/main/java/es/etg/prog/pokedex/controller/pokedexcontroller.java
+++ src/main/java/es/etg/prog/pokedex/controller/pokedexcontroller.java
@@ -1,5 +1,24 @@
 package es.etg.prog.pokedex.controller;
+import es.etg.prog.pokedex.view.PokedexView;
 
-public class pokedexcontroller {
-    
+public class PokedexController { 
+    private final PokedexView vista;
+
+    public PokedexController(PokedexView vista){
+        this.vista = vista;
+    }
+
+    public void init(){
+        int opcion;
+        do { 
+            vista.mostrarMenu();
+            opcion = vista.leerEntero();
+            switch (opcion) {
+                case 1 -> vista.leerString();
+                case 2 -> vista.leerEntero();
+                case 3 -> {}
+                default -> throw new AssertionError();
+            }
+        } while (opcion != 3);
+    }
 }
