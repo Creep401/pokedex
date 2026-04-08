e338e84b350967dded9cfa7d237492bbff6a1387
Creep401 <diegotadeo711@gmail.com>
2026-04-08 15:29:21 +0200

    Implementado pokemon

---
+++ src/main/java/es/etg/prog/pokedex/model/dto/Pokemon.java
@@ -0,0 +1,9 @@
+package es.etg.prog.pokedex.model.dto;
+
+public class Pokemon {
+    public enum Tipo {
+        Planta, Fuego, Agua, Electrico, Hielo, Lucha, Veneno, Tierra, Volador, Psiquico, Bicho, Roca, Fantasma, Dragon, Siniestro, Acero, Hada
+    }
+
+    private Tipo tipo;
+}
