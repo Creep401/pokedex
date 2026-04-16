e557b70ce5a3c55ba3190fca57ff6bfc85d1d4cd
Creep401 <diegotadeo711@gmail.com>
2026-04-16 18:35:15 +0200

    Agregado pokemon

--- src/main/java/es/etg/prog/pokedex/model/dto/Pokemon.java
+++
@@ -1,9 +0,0 @@
-package es.etg.prog.pokedex.model.dto;
-
-public class Pokemon {
-    public enum Tipo {
-        Planta, Fuego, Agua, Electrico, Hielo, Lucha, Veneno, Tierra, Volador, Psiquico, Bicho, Roca, Fantasma, Dragon, Siniestro, Acero, Hada
-    }
-
-    private Tipo tipo;
-}
